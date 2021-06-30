d3f2f611d2079c43a033ebf39b97d904e0b08f96
Vojta188 <75996784+Vojta188@users.noreply.github.com>
2021-06-30 17:18:28 +0200

    Update connect.php

--- connect.php
+++ connect.php
@@ -14,10 +14,9 @@ $htmlodeslat = "Contact.html";
 
 
 
-// database insert SQL code
 $sql = "INSERT INTO `tbl_contact` (`Id`, `fldName`, `fldEmail`, `fldMessage`) VALUES ('0', '$txtName', '$txtEmail', '$txtMessage')";
 
-// insert in database 
+
 $rs = mysqli_query($con, $sql);
 
 if($rs)
